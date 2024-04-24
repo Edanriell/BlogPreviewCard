dc205eac403c01ffe51bfb0a497e80b9cc4cb6d8
Edanriell <Edanriell@gmail.com>
2024-04-25 00:42:26 +0300

    added font and configured tailwind

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -9,13 +9,70 @@ const config: Config = {
 		"./src/widgets/**/*.{js,ts,jsx,tsx}"
 	],
 	theme: {
+		fontFamily: {
+			figtree: ["Figtree"]
+		},
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+			colors: {
+				codGray: {
+					"50": "#f6f6f6",
+					"100": "#e7e7e7",
+					"200": "#d1d1d1",
+					"300": "#b0b0b0",
+					"400": "#888888",
+					"500": "#6d6d6d",
+					"600": "#5d5d5d",
+					"700": "#4f4f4f",
+					"800": "#454545",
+					"900": "#3d3d3d",
+					"950": "#111111"
+				},
+				white: {
+					"50": "#ffffff",
+					"100": "#efefef",
+					"200": "#dcdcdc",
+					"300": "#bdbdbd",
+					"400": "#989898",
+					"500": "#7c7c7c",
+					"600": "#656565",
+					"700": "#525252",
+					"800": "#464646",
+					"900": "#3d3d3d",
+					"950": "#292929"
+				},
+				dandelion: {
+					"50": "#fefaec",
+					"100": "#fcf3c9",
+					"200": "#f8e58f",
+					"300": "#f4d04e",
+					"400": "#f2be2d",
+					"500": "#eb9f15",
+					"600": "#d0790f",
+					"700": "#ad5710",
+					"800": "#8c4314",
+					"900": "#733814",
+					"950": "#421c06"
+				},
+				gray: {
+					"50": "#f6f6f6",
+					"100": "#e7e7e7",
+					"200": "#d1d1d1",
+					"300": "#b0b0b0",
+					"400": "#7f7f7f",
+					"500": "#6d6d6d",
+					"600": "#5d5d5d",
+					"700": "#4f4f4f",
+					"800": "#454545",
+					"900": "#3d3d3d",
+					"950": "#262626"
+				}
+			},
+			boxShadow: {
+				postCard: "8px 8px 0 0 #000"
 			}
 		}
 	},
 	plugins: [require("@tailwindcss/forms")]
 };
+
 export default config;
