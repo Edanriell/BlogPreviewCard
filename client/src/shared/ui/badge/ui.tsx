c2a47e967324ee110aba4349a6de7ca58507cea0
Edanriell <Edanriell@gmail.com>
2024-04-26 21:49:15 +0300

    badge finished

--- client/src/shared/ui/badge/ui.tsx
+++ client/src/shared/ui/badge/ui.tsx
@@ -1,3 +1,21 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
-export const Badge: FC = () => null;
+type BadgeProps = {
+	color?: string;
+	children?: ReactNode;
+};
+
+export const Badge: FC<BadgeProps> = ({ color, children }) => {
+	const badgeColorClass = color ? `bg-[${color}]` : "bg-dandelion-300";
+
+	return (
+		<p
+			className={
+				badgeColorClass +
+				" rounded-[0.4rem] px-[1.2rem] py-[0.4rem] font-figtree font-extrabold text-[1.2rem] md:text-[1.4rem] text-codGray-950 text-left inline"
+			}
+		>
+			{children}
+		</p>
+	);
+};
