743a13f4d479d25a60173177205722b5a55c4c08
Edanriell <Edanriell@gmail.com>
2024-04-27 13:45:39 +0300

    finished link ccomponent

---
+++ client/src/shared/ui/link/ui.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { FC, ReactNode, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+type LinkProps = {
+	href: string;
+	color?: string;
+	activeColor?: string;
+	children?: ReactNode;
+};
+
+export const Link: FC<LinkProps> = ({
+	href,
+	color = "#111111",
+	activeColor = "#f4d04e",
+	children
+}) => {
+	const linkRef = useRef<HTMLAnchorElement>(null);
+	const { contextSafe } = useGSAP();
+
+	useGSAP(() => {
+		gsap.set(linkRef.current, { color: color });
+	});
+
+	const handleLinkMouseEnter = contextSafe(() => {
+		gsap.to(linkRef.current, { color: activeColor, ease: "power2.out", duration: 0.25 });
+	});
+
+	const handleLinkMouseLeave = contextSafe(() => {
+		gsap.to(linkRef.current, { color: color, ease: "power2.out", duration: 0.5 });
+	});
+
+	const handleLinkTouchStart = contextSafe(() => {
+		gsap.to(linkRef.current, { color: activeColor, ease: "power2.out", duration: 0.25 });
+	});
+
+	const handleLinkTouchEnd = contextSafe(() => {
+		gsap.to(linkRef.current, { color: color, ease: "power2.out", duration: 0.5 });
+	});
+	
+	return (
+		<a
+			ref={linkRef}
+			onMouseEnter={handleLinkMouseEnter}
+			onMouseLeave={handleLinkMouseLeave}
+			onTouchStart={handleLinkTouchStart}
+			onTouchEnd={handleLinkTouchEnd}
+			className={"block"}
+			href={href}
+		>
+			{children}
+		</a>
+	);
+};
